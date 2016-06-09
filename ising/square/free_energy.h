f917cd5213b8441d68539ec9bd58421f687ed98c
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2016-06-09 22:31:56 +0900

    Spped up ising::square::free_energy_density in the thermodynamic limit

--- ising/square/free_energy.h
+++ ising/square/free_energy.h
@@ -22,20 +22,22 @@
 
 // Calculating free energy density of square lattice Ising model in the thermodynamic limit
 
-// reference: B. Kastening, Phys. Rev. E 64, 066106 (2001), wrn:2011/02/10
+// reference: https://en.wikipedia.org/wiki/Square-lattice_Ising_model
 
 #ifndef ISING_SQUARE_FREE_ENERGY_H
 #define ISING_SQUARE_FREE_ENERGY_H
 
+#include <boost/throw_exception.hpp>
 #include <iostream>
 #include <cmath>
+#include <stdexcept>
 
 namespace {
 
-inline double f(double theta, double Ka, double Kb) {
-  double k = 1.0 / (std::sinh(2 * Ka) * std::sinh(2 * Kb));
-  return std::log(std::cosh(2 * Ka) * std::cosh(2 * Kb) +
-                  std::sqrt(1 + k * k - 2 * k * std::cos(2 * theta)) / k);
+// ch2a = cosh(2 * beta * Ja), sh2a = sinh(2 * beta * Ja), etc
+inline double f(double theta, double ch2a, double ch2b, double sh2a, double sh2b) {
+  double k = 1.0 / (sh2a * sh2b);
+  return std::log(ch2a * ch2b + std::sqrt(1 + k * k - 2 * k * std::cos(2 * theta)) / k);
 }
 
 }
@@ -43,15 +45,28 @@ inline double f(double theta, double Ka, double Kb) {
 namespace ising {
 namespace square {
   
-inline double free_energy_density(double beta, double Ja, double Jb, int Nint = 100) {
-  double Ka = beta * Ja;
-  double Kb = beta * Jb;
-  double g = 0.0;
+inline double free_energy_density(double beta, double Ja, double Jb, unsigned int Nint) {
+  if (beta <= 0)
+    boost::throw_exception(std::invalid_argument("beta should be positive"));
+  if (Nint == 0 || (Nint % 2) != 0)
+    boost::throw_exception(std::invalid_argument("Nint should be positive and a multiple of two"));
+  double ch2a = std::cosh(2 * beta * Ja);
+  double ch2b = std::cosh(2 * beta * Jb);
+  double sh2a = std::sinh(2 * beta * Ja);
+  double sh2b = std::sinh(2 * beta * Jb);
   double dt = M_PI / Nint;
+  double g = 0.0;
+  // i == 0 || i == Nint
+  g += f(0, ch2a, ch2b, sh2a, sh2b) + f(M_PI, ch2a, ch2b, sh2a, sh2b);
+  // i = 1/2 ... Nint-1/2
   for (int i = 0; i < Nint; ++i) {
-    double t0 = dt * i;
-    double t1 = dt * (i+1);
-    g += f(t0, Ka, Kb) + 4 * f((t0 + t1) / 2, Ka, Kb) + f(t1, Ka, Kb);
+    double t = dt * (i + 0.5);
+    g += 4 * f(t, ch2a, ch2b, sh2a, sh2b);
+  }
+  // i = 1 ... Nint-1
+  for (int i = 1; i < Nint; ++i) {
+    double t = dt * i;
+    g += 2 * f(t, ch2a, ch2b, sh2a, sh2b);
   }
   g *= dt / 6;
   g /= 2 * M_PI;
