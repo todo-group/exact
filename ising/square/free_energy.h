125c6e34b7bc21505a2efbadff2c4e18603303df
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2016-06-13 12:42:33 +0900

    Factorized Simpson integration into include/simpson_integration.h

--- ising/square/free_energy.h
+++ ising/square/free_energy.h
@@ -14,17 +14,24 @@
 #ifndef ISING_SQUARE_FREE_ENERGY_H
 #define ISING_SQUARE_FREE_ENERGY_H
 
+#include "simpson_integration.h"
 #include <boost/throw_exception.hpp>
 #include <cmath>
 #include <stdexcept>
 
 namespace {
 
-// ch2a = cosh(2 * beta * Ja), sh2a = sinh(2 * beta * Ja), etc
-inline double f(double theta, double ch2a, double ch2b, double sh2a, double sh2b) {
-  double k = 1.0 / (sh2a * sh2b);
-  return std::log(ch2a * ch2b + std::sqrt(1 + k * k - 2 * k * std::cos(2 * theta)) / k);
-}
+struct func {
+  func(double beta, double Ja, double Jb) {
+    c_ = 1.0 / (2 * M_PI);
+    chab_ = std::cosh(2 * beta * Ja) * std::cosh(2 * beta * Jb);
+    k_ = 1.0 / (std::sinh(2 * beta * Ja) * std::sinh(2 * beta * Jb));
+  }
+  double operator()(double t) const {
+    return c_ * std::log(chab_ + std::sqrt(1 + k_ * k_ - 2 * k_ * std::cos(2 * t)) / k_);
+  }
+  double c_, chab_, k_;
+};
 
 }
 
@@ -34,29 +41,8 @@ namespace square {
 inline double free_energy_density(double beta, double Ja, double Jb, unsigned int Nint) {
   if (beta <= 0)
     boost::throw_exception(std::invalid_argument("beta should be positive"));
-  if (Nint == 0 || (Nint % 2) != 0)
-    boost::throw_exception(std::invalid_argument("Nint should be positive and a multiple of two"));
-  double ch2a = std::cosh(2 * beta * Ja);
-  double ch2b = std::cosh(2 * beta * Jb);
-  double sh2a = std::sinh(2 * beta * Ja);
-  double sh2b = std::sinh(2 * beta * Jb);
-  double dt = M_PI / Nint;
-  double g = 0.0;
-  // i == 0 || i == Nint
-  g += f(0, ch2a, ch2b, sh2a, sh2b) + f(M_PI, ch2a, ch2b, sh2a, sh2b);
-  // i = 1/2 ... Nint-1/2
-  for (int i = 0; i < Nint; ++i) {
-    double t = dt * (i + 0.5);
-    g += 4 * f(t, ch2a, ch2b, sh2a, sh2b);
-  }
-  // i = 1 ... Nint-1
-  for (int i = 1; i < Nint; ++i) {
-    double t = dt * i;
-    g += 2 * f(t, ch2a, ch2b, sh2a, sh2b);
-  }
-  g *= dt / 6;
-  g /= 2 * M_PI;
-  return - (std::log(2.0) / 2 + g) / beta;
+  func f(beta, Ja, Jb);
+  return - (std::log(2.0) / 2 + simpson_integration_1d(f, 0, M_PI, Nint)) / beta;
 }
 
 } // end namespace square
--- ising/triangle/free_energy.h
+++ ising/triangle/free_energy.h
@@ -14,106 +14,39 @@
 #ifndef ISING_TRIANGLE_FREE_ENERGY_H
 #define ISING_TRIANGLE_FREE_ENERGY_H
 
+#include "simpson_integration.h"
 #include <boost/throw_exception.hpp>
 #include <cmath>
 #include <stdexcept>
 
 namespace {
 
-// ch2a = cosh(2 * beta * Ja), sh2a = sinh(2 * beta * Ja), etc
-inline double f(double omega1, double omega2, double ch2a, double ch2b, double ch2c,
-                double sh2a, double sh2b, double sh2c) {
-  return std::log(ch2a * ch2b * ch2c + sh2a * sh2b * sh2c
-                  - sh2a * std::cos(omega1) - sh2b * std::cos(omega2)
-                  - sh2c * std::cos(omega1 + omega2));
-}
+struct func {
+  func(double beta, double Ja, double Jb, double Jc) {
+    c_ = 1.0 / (8 * M_PI * M_PI);
+    sh2a_ = std::sinh(2 * beta * Ja);
+    sh2b_ = std::sinh(2 * beta * Jb);
+    sh2c_ = std::sinh(2 * beta * Jc);
+    cshabc_ = std::cosh(2 * beta * Ja) * std::cosh(2 * beta * Jb) * std::cosh(2 * beta * Jc)
+      + sh2a_ * sh2b_ * sh2c_;
+  }
+  double operator()(double t1, double t2) const {
+    return c_ * std::log(cshabc_ - sh2a_ * std::cos(t1) - sh2b_ * std::cos(t2)
+                         - sh2c_ * std::cos(t1 + t2));
+  }
+  double c_, sh2a_, sh2b_, sh2c_, cshabc_;
+};
 
 }
 
 namespace ising {
 namespace triangle {
   
-inline double free_energy_density(double beta, double Ja, double Jb, double Jc, int Nint = 100) {
-  double ch2a = std::cosh(2 * beta * Ja);
-  double ch2b = std::cosh(2 * beta * Jb);
-  double ch2c = std::cosh(2 * beta * Jc);
-  double sh2a = std::sinh(2 * beta * Ja);
-  double sh2b = std::sinh(2 * beta * Jb);
-  double sh2c = std::sinh(2 * beta * Jc);
-  double domega = 2 * M_PI / Nint;
-  double g = 0.0;
-  // i == 0
-  {
-    double omega1 = 0;
-    //   j == 0 || i == Nint
-    g += f(omega1, 0, ch2a, ch2b, ch2c, sh2a, sh2b, sh2c)
-      + f(omega1, 2 * M_PI, ch2a, ch2b, ch2c, sh2a, sh2b, sh2c);
-    //   j = 1/2 ... Nint-1/2
-    for (int j = 0; j < Nint; ++j) {
-      double omega2 = domega * (j + 0.5);
-      g += 4 * f(omega1, omega2, ch2a, ch2b, ch2c, sh2a, sh2b, sh2c);
-    }
-    //   j = 1 ... Nint-1
-    for (int j = 1; j < Nint; ++j) {
-      double omega2 = domega * j;
-      g += 2 * f(omega1, omega2, ch2a, ch2b, ch2c, sh2a, sh2b, sh2c);
-    }
-  }
-  // i = 1/2 ... Nint-1/2
-  for (int i = 0; i < Nint; ++i) {
-    double omega1 = domega * (i + 0.5);
-    //   j == 0 || i == Nint
-    g += 4 * f(omega1, 0, ch2a, ch2b, ch2c, sh2a, sh2b, sh2c)
-      + 4 * f(omega1, 2 * M_PI, ch2a, ch2b, ch2c, sh2a, sh2b, sh2c);
-    //   j = 1/2 ... Nint-1/2
-    for (int j = 0; j < Nint; ++j) {
-      double omega2 = domega * (j + 0.5);
-      g += 16 * f(omega1, omega2, ch2a, ch2b, ch2c, sh2a, sh2b, sh2c);
-    }
-    //   j = 1 ... Nint-1
-    for (int j = 1; j < Nint; ++j) {
-      double omega2 = domega * j;
-      g += 8 * f(omega1, omega2, ch2a, ch2b, ch2c, sh2a, sh2b, sh2c);
-    }
-  }
-  // i = 1 ... Nint-1
-  for (int i = 1; i < Nint; ++i) {
-    double omega1 = domega * i;
-    //   j == 0 || i == Nint
-    g += 2 * f(omega1, 0, ch2a, ch2b, ch2c, sh2a, sh2b, sh2c)
-      + 2 * f(omega1, 2 * M_PI, ch2a, ch2b, ch2c, sh2a, sh2b, sh2c);
-    //   j = 1/2 ... Nint-1/2
-    for (int j = 0; j < Nint; ++j) {
-      double omega2 = domega * (j + 0.5);
-      g += 8 * f(omega1, omega2, ch2a, ch2b, ch2c, sh2a, sh2b, sh2c);
-    }
-    //   j = 1 ... Nint-1
-    for (int j = 1; j < Nint; ++j) {
-      double omega2 = domega * j;
-      g += 4 * f(omega1, omega2, ch2a, ch2b, ch2c, sh2a, sh2b, sh2c);
-    }
-  }
-  // i == Nint
-  {
-    double omega1 = 2 * M_PI;
-    //   j == 0 || i == Nint
-    g += f(omega1, 0, ch2a, ch2b, ch2c, sh2a, sh2b, sh2c)
-      + f(omega1, 2 * M_PI, ch2a, ch2b, ch2c, sh2a, sh2b, sh2c);
-    //   j = 1/2 ... Nint-1/2
-    for (int j = 0; j < Nint; ++j) {
-      double omega2 = domega * (j + 0.5);
-      g += 4 * f(omega1, omega2, ch2a, ch2b, ch2c, sh2a, sh2b, sh2c);
-    }
-    //   j = 1 ... Nint-1
-    for (int j = 1; j < Nint; ++j) {
-      double omega2 = domega * j;
-      g += 2 * f(omega1, omega2, ch2a, ch2b, ch2c, sh2a, sh2b, sh2c);
-    }
-  }
-
-  g *= domega * domega / 36;
-  g /= 8 * M_PI * M_PI;
-  return - (std::log(2.0) + g) / beta;
+inline double free_energy_density(double beta, double Ja, double Jb, double Jc, int Nint) {
+  if (beta <= 0)
+    boost::throw_exception(std::invalid_argument("beta should be positive"));
+  func f(beta, Ja, Jb, Jc);
+  return - (std::log(2.0) + simpson_integration_2d(f, 0, 0, 2 * M_PI, 2 * M_PI, Nint, Nint)) / beta;
 }
 
 } // end namespace triangle
