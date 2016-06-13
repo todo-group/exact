b9e2acd27736326876fe14043ac3311127600ced
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2016-06-13 11:46:24 +0900

    Changed output precision

--- ising/triangle/free_energy.h
+++ ising/triangle/free_energy.h
@@ -1,22 +1,9 @@
 /*****************************************************************************
 *
-* Copyright (C) 2011-2016 by Synge Todo <wistaria@comp-phys.org>
+* Copyright (C) 2015-2016 by Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
 *
-* This software is published under the ALPS Application License; you
-* can use, redistribute it and/or modify it under the terms of the
-* license, either version 1 or (at your option) any later version.
-* 
-* You should have received a copy of the ALPS Application License
-* along with this software; see the file LICENSE. If not, the license
-* is also available from http://alps.comp-phys.org/.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-* FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT 
-* SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE 
-* FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE, 
-* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-* DEALINGS IN THE SOFTWARE.
+* Distributed under the Boost Software License, Version 1.0. (See accompanying
+* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 *
 *****************************************************************************/
 
@@ -28,7 +15,6 @@
 #define ISING_TRIANGLE_FREE_ENERGY_H
 
 #include <boost/throw_exception.hpp>
-#include <iostream>
 #include <cmath>
 #include <stdexcept>
 
