7164c968eaa6e5befc3290cae684d9b585755ae0
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2017-12-18 11:25:34 +0900

    Define EXACT_INCLUDE_DIRS in exact-config.cmake

--- config/use-exact.cmake
+++ config/use-exact.cmake
@@ -1,10 +1,10 @@
 #
-# This module is provided as ROKKO_USE_FILE by exact-config.cmake.  It can
-# be included in a project to load the needed compiler and linker
-# settings to use Exact
+# This module is provided as EXACT_USE_FILE by exact-config.cmake.
+# It can be included in a project to load the needed compiler and
+# linker settings to use "Exact".
 #
 
 if(NOT EXACT_USE_FILE_INCLUDED)
   set(EXACT_USE_FILE_INCLUDED 1)
-  include_directories(${EXACT_ROOT_DIR}/include)
+  include_directories(${EXACT_INCLUDE_DIRS})
 endif(NOT EXACT_USE_FILE_INCLUDED)
