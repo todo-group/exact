06bd745eac19853f1a5308f202e830356b2a8cce
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2021-02-22 18:31:58 +0900

    using boost::multiprecision

--- ising/dos/square_finite.cpp
+++ ising/dos/square_finite.cpp
@@ -16,6 +16,9 @@
 #include <boost/math/constants/constants.hpp>
 #include <boost/math/differentiation/autodiff.hpp>
 #include <boost/math/special_functions/binomial.hpp>
+#include <boost/math/special_functions/factorials.hpp>
+#include <boost/multiprecision/cpp_dec_float.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
 
 template<typename T>
 T zero(const T& x) { return 0 * x; }
@@ -37,35 +40,71 @@ T alpha(const T& x, const T& beta, unsigned n, unsigned k) {
   return power_n(1 + x * x, 2) - beta * cos(pi * k / n);
 }
 
-template<typename T>
-T c2(const T& x, const T& beta, unsigned m, unsigned n, unsigned k) {
+template<unsigned Order, unsigned Digits10, class ExponentType = boost::int32_t>
+void calc_dos(unsigned m, unsigned n) {
   using namespace boost::math;
-  auto ak = alpha(x, beta, n, k);
-  auto res = zero(x);
-  for (unsigned j = 0; j <= m; j += 2)
-    res += binomial_coefficient<typename T::root_type>(m, j)
-      * power_n(ak * ak - beta * beta, j/2) * power_n(ak, m-j);
-  res += power_n(beta, m);
-  return res / power_n(2, m - 1);
-}
+  using namespace boost::math::differentiation;
+  namespace mp = boost::multiprecision;
 
-template<typename T>
-T s2(const T& x, const T& beta, unsigned m, unsigned n, unsigned k) {
-  using namespace boost::math;
-  auto ak = alpha(x, beta, n, k);
-  auto res = zero(x);
-  for (unsigned j = 0; j <= m; j += 2)
-    res += binomial_coefficient<typename T::root_type>(m, j)
-      * power_n(ak * ak - beta * beta, j/2) * power_n(ak, m-j);
-  res -= power_n(beta, m);
-  return res / power_n(2, m - 1);
+  typedef mp::cpp_int int_type;
+  typedef mp::number<mp::cpp_dec_float<Digits10, ExponentType>> real_type;
+  std::cout << std::setprecision(std::numeric_limits<real_type>::max_digits10);
+
+  auto const x = make_fvar<real_type, Order>(0);
+  auto const xpo = make_fvar<real_type, Order>(-1);
+  auto const xmo = make_fvar<real_type, Order>(+1);
+
+  auto beta = 2 * x * xpo * xmo;
+  auto beta_m = power_n(beta, m);
+  auto c0 = power_n(xmo, m) + power_n(x * xpo, m);
+  auto s0 = power_n(xmo, m) - power_n(x * xpo, m);
+  auto cn = power_n(xpo, m) + power_n(x * xmo, m);
+  auto sn = power_n(xpo, m) - power_n(x * xmo, m);
+
+  auto z1 = zero(x);
+  auto z2 = zero(x);
+  auto z3 = zero(x);
+  auto z4 = zero(x);
+  if ((n & 1) == 0) {
+    z1 = one(x) / 2;
+    z2 = one(x) / 2;
+    z3 = c0 * cn / 2;
+    z4 = s0 * sn / 2;
+  } else {
+    z1 = cn / 2;
+    z2 = sn / 2;
+    z3 = c0 / 2;
+    z4 = s0 / 2;
+  }
+  for (unsigned k = 1; k < n; ++k) {
+    auto ak = alpha(x, beta, n, k);
+    auto v = zero(x);
+    for (unsigned j = 0; j <= m; j += 2)
+      v += binomial_coefficient<real_type>(m, j)
+        * power_n(ak * ak - beta * beta, j/2) * power_n(ak, m-j);
+    if ((k & 1) == 1) {
+      z1 *= (v + beta_m) / power_n(real_type(2), m - 1);
+      z2 *= (v - beta_m) / power_n(real_type(2), m - 1);
+    } else {
+      z3 *= (v + beta_m) / power_n(real_type(2), m - 1);
+      z4 *= (v - beta_m) / power_n(real_type(2), m - 1);
+    }
+  }
+  auto zmn = z1 + z2 + z3 + z4;
+
+  // checking sum rule
+  int_type sum;
+  for (unsigned i = 0; i <= 2 * m * n; i += 2) sum += int_type(zmn.at(i) + 0.01);
+  if (sum != power_n(int_type(2), m * n)) {
+    std::cerr << "Error: result check failed\n";
+    throw(0);
+  }
+
+  for (unsigned i = 0; i <= 2 * m * n; i += 2) std::cout << int_type(zmn.at(i) + 0.01) << ' ';
+  std::cout << std::endl;
 }
 
 int main(int argc, char **argv) {
-  using namespace boost::math;
-  using namespace boost::math::differentiation;
-
-  constexpr unsigned ORDER = 128;
   unsigned m, n;
   try {
     if (argc == 2) {
@@ -88,38 +127,16 @@ int main(int argc, char **argv) {
     return 127;
   }
 
-  auto const x = make_fvar<double, ORDER>(0.0);
-
-  auto beta = 2 * x * (1 - x * x);
-  auto c0 = power_n(1 - x, m) + power_n(x * (1 + x), m);
-  auto s0 = power_n(1 - x, m) - power_n(x * (1 + x), m);
-  auto cn = power_n(1 + x, m) + power_n(x * (1 - x), m);
-  auto sn = power_n(1 + x, m) - power_n(x * (1 - x), m);
-
-  auto z1 = zero(x);
-  auto z2 = zero(x);
-  auto z3 = zero(x);
-  auto z4 = zero(x);
-  if ((n & 1) == 0) {
-    z1 = z2 = one(x) / 2;
-    z3 = c0 * cn / 2;
-    z4 = s0 * sn / 2;
+  if (2 * m * n <= 128) {
+    calc_dos<128, 100>(m, n);
+  } else if (2 * m * n <= 512) {
+    calc_dos<512, 100>(m, n);
+  } else if (2 * m * n <= 2048) {
+    calc_dos<2048, 100>(m, n);
+  } else if (2 * m * n <= 4608) {
+    calc_dos<4608, 100>(m, n);
   } else {
-    z1 = cn / 2;
-    z2 = sn / 2;
-    z3 = c0 / 2;
-    z4 = s0 / 2;
+    std::cerr << "Error: m * n is too large\n";
+    return 127;
   }
-  for (unsigned k = 1; k < n; ++k) {
-    if ((k & 1) == 1) {
-      z1 *= c2(x, beta, m, n, k);
-      z2 *= s2(x, beta, m, n, k);
-    } else {
-      z3 *= c2(x, beta, m, n, k);
-      z4 *= s2(x, beta, m, n, k);
-    }
-  }
-  auto zmn = z1 + z2 + z3 + z4;
-  for (unsigned i = 0; i <= 2 * m * n; i += 2) std::cout << unsigned(zmn.at(i) + 0.5) << ' ';
-  std::cout << std::endl;
 }
