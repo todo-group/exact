d952768ab7b06caaa43dcf551aa3267d99b46148
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2021-07-22 12:37:32 +0900

    refactor: ising/tc ising/dos

--- ising/tc/square.cpp
+++ ising/tc/square.cpp
@@ -1,11 +1,18 @@
-/*****************************************************************************
-*
-* Copyright (C) 2015-2021 by Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
-*
-* Distributed under the Boost Software License, Version 1.0. (See accompanying
-* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-*
-*****************************************************************************/
+/*
+   Copyright (C) 2015-2021 by Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 
 // Critical temperature of square lattice Ising model
 
@@ -13,39 +20,9 @@
 #include <iostream>
 #include <boost/multiprecision/cpp_dec_float.hpp>
 #include "ising/mp_wrapper.hpp"
+#include "options.hpp"
 #include "square.hpp"
 
-struct options {
-  unsigned int prec;
-  std::string Jx, Jy;
-  bool valid;
-  options(unsigned int argc, char *argv[]) :
-    prec(15), Jx("1"), Jy("1"), valid(true) {
-    for (unsigned i = 1; i < argc; ++i) {
-      switch (argv[i][0]) {
-      case '-' :
-        switch (argv[i][1]) {
-        case 'p' :
-          if (++i == argc) { valid = false; return; }
-          prec = atoi(argv[i]); break;
-        default :
-          valid = false; return;
-        }
-        break;
-      default :
-        switch (argc - i) {
-        case 1:
-          Jx = Jy = argv[i]; return;
-        case 2:
-          Jx = argv[i]; Jy = argv[i+1]; return;
-        default:
-          valid = false; return;
-        }
-      }
-    }
-  }
-};
-  
 template<class T>
 void calc(const std::string& Jx_in, const std::string& Jy_in) {
   typedef T real_t;
@@ -61,10 +38,8 @@ void calc(const std::string& Jx_in, const std::string& Jy_in) {
 
 int main(int argc, char **argv) {
   namespace mp = boost::multiprecision;
-  options opt(argc, argv);
-  if (!opt.valid) {
-    std::cerr << "Usage: " << argv[0] << " [-p prec] [Jx [Jy]]\n"; return 127;
-  }
+  options2 opt(argc, argv);
+  if (!opt.valid) return 127;
   if (opt.prec <= std::numeric_limits<float>::digits10) {
     calc<float>(opt.Jx, opt.Jy);
   } else if (opt.prec <= std::numeric_limits<double>::digits10) {
--- ising/tc/triangular.cpp
+++ ising/tc/triangular.cpp
@@ -1,11 +1,18 @@
-/*****************************************************************************
-*
-* Copyright (C) 2015-2021 by Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
-*
-* Distributed under the Boost Software License, Version 1.0. (See accompanying
-* file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-*
-*****************************************************************************/
+/*
+   Copyright (C) 2015-2021 by Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 
 // Critical temperature of triangular lattice Ising model
 
@@ -13,38 +20,8 @@
 #include <iostream>
 #include <boost/multiprecision/cpp_dec_float.hpp>
 #include "ising/mp_wrapper.hpp"
+#include "options.hpp"
 #include "triangular.hpp"
-
-struct options {
-  unsigned int prec;
-  std::string Ja, Jb, Jc;
-  bool valid;
-  options(unsigned int argc, char *argv[]) :
-    prec(15), Ja("1"), Jb("1"), Jc("1"), valid(true) {
-    for (unsigned i = 1; i < argc; ++i) {
-      switch (argv[i][0]) {
-      case '-' :
-        switch (argv[i][1]) {
-        case 'p' :
-          if (++i == argc) { valid = false; return; }
-          prec = atoi(argv[i]); break;
-        default :
-          valid = false; return;
-        }
-        break;
-      default :
-        switch (argc - i) {
-        case 1:
-          Ja = Jb = Jc = argv[i]; return;
-        case 3:
-          Ja = argv[i]; Jb = argv[i+1]; Jc = argv[i+2]; return;
-        default:
-          valid = false; return;
-        }
-      }
-    }
-  }
-};
 
 template<class T>
 void calc(const std::string& Ja_in, const std::string& Jb_in, const std::string& Jc_in) {
@@ -62,10 +39,8 @@ void calc(const std::string& Ja_in, const std::string& Jb_in, const std::string&
 
 int main(int argc, char **argv) {
   namespace mp = boost::multiprecision;
-  options opt(argc, argv);
-  if (!opt.valid) {
-    std::cerr << "Usage: " << argv[0] << " [-p prec] [Ja [Jb Jc]]\n"; return 127;
-  }
+  options3 opt(argc, argv);
+  if (!opt.valid) return 127;
   if (opt.prec <= std::numeric_limits<float>::digits10) {
     calc<float>(opt.Ja, opt.Jb, opt.Jc);
   } else if (opt.prec <= std::numeric_limits<double>::digits10) {
