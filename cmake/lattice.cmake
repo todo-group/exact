d7ba90606051176b4399919dd0072e39bc72b361
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2021-12-08 10:20:31 +0900

    update

--- cmake/lattice.cmake
+++ cmake/lattice.cmake
@@ -7,3 +7,5 @@ FetchContent_Declare(
   lattice
   GIT_REPOSITORY https://github.com/todo-group/lattice.git
 )
+list(APPEND FetchContent_includes "${PROJECT_BINARY_DIR}/_deps/lattice-src")
+list(APPEND FetchContents lattice)
