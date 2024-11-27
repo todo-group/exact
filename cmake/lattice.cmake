4a97ae0e8ef0bbff6069daf36ca17e1299a89814
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2024-11-27 19:51:02 +0900

    cmake: update

--- cmake/lattice.cmake
+++ cmake/lattice.cmake
@@ -1,11 +1,7 @@
-option(LATTICE_INSTALL OFF)
-option(LATTICE_BUILD_TESTS OFF)
-option(LATTICE_BUILD_EXAMPLES OFF)
-
 include(FetchContent)
 FetchContent_Declare(
   lattice
   GIT_REPOSITORY https://github.com/todo-group/lattice.git
+  GIT_TAG        master
 )
-list(APPEND FetchContent_includes "${PROJECT_BINARY_DIR}/_deps/lattice-src")
 list(APPEND FetchContents lattice)
--- cmake/standards.cmake
+++ cmake/standards.cmake
@@ -1,11 +1,7 @@
-option(STANDARDS_INSTALL OFF)
-option(STANDARDS_BUILD_TESTS OFF)
-option(STANDARDS_BUILD_EXAMPLES OFF)
-
 include(FetchContent)
 FetchContent_Declare(
   standards
   GIT_REPOSITORY https://github.com/todo-group/standards.git
+  GIT_TAG        master
 )
-list(APPEND FetchContent_includes "${PROJECT_BINARY_DIR}/_deps/standards-src")
 list(APPEND FetchContents standards)
